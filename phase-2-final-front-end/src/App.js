ed471c10e61519e87fc4be7afbf3ad731e1ee9ca
Candice Thomason <candice.thomason@gmail.com>
2024-06-07 08:42:12 +1000

    update app.js for Loginsignup

--- phase-2-final-front-end/src/App.js
+++ phase-2-final-front-end/src/App.js
@@ -1,16 +1,11 @@
 import React from "react";
 import "./App.css";
-import LoginBox from "./Components/LoginBox";
-import NewUser from "./Components/NewUser";
+import LoginSignup from "./Components/LoginSignup/LoginSignup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Casuals Pro</h1>
-        <LoginBox></LoginBox>
-        <NewUser></NewUser>
-      </header>
+    <div>
+      <LoginSignup />
     </div>
   );
 }
---
+++ phase-2-final-front-end/src/Components/LoginSignup/LoginSignup.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ".?/LoginSignup.css";
+
+const LoginSignup = () => {
+  return <div></div>;
+};
+export default LoginSignup;
