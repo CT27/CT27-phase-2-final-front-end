7b067c1f2ea3bfcc02d7611112a610fe19589197
Candice Thomason <candice.thomason@gmail.com>
2024-06-07 09:30:49 +1000

    add chnages

--- phase-2-final-front-end/src/Components/LoginSignup/LoginSignup.js
+++ phase-2-final-front-end/src/Components/LoginSignup/LoginSignup.js
@@ -1,7 +1,26 @@
 import React from "react";
-import ".?/LoginSignup.css";
+import "./LoginSignup.css";
+import { HiOutlineMail } from "react-icons/hi";
+import { HiOutlineUser } from "react-icons/hi";
+import { HiOutlineKey } from "react-icons/hi";
 
 const LoginSignup = () => {
-  return <div></div>;
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="text">SignUp</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        <div className="input">
+          <img src="" alt="" />
+          <p>
+            here is an email icon <HiOutlineMail />
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
+
 export default LoginSignup;
