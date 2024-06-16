44461b70bfb922843c2374e78a95d0cf2e6f50b6
Candice Thomason <candice.thomason@gmail.com>
2024-06-17 09:43:52 +1000

    completed login component and css front end only

--- phase-2-final-front-end/src/Components/LoginSignup/LoginSignup.js
+++ phase-2-final-front-end/src/Components/LoginSignup/LoginSignup.js
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginSignup.css";
 import { HiOutlineMail } from "react-icons/hi";
 import { HiOutlineUser } from "react-icons/hi";
 import { HiOutlineKey } from "react-icons/hi";
 
 const LoginSignup = () => {
+  const [action, setAction] = useState("Login");
+
   return (
     <div className="container">
       <div className="header">
-        <div className="text">SignUp</div>
+        <div className="text">{action}</div>
         <div className="underline"></div>
       </div>
       <div className="inputs">
-        <div className="input">
-          <HiOutlineUser className="custom-icon" />
-          <input type="text" placeholder="Name" />
-        </div>
+        {action === "Login" ? (
+          <div></div>
+        ) : (
+          <div className="input">
+            <HiOutlineUser className="custom-icon" />
+            <input type="text" placeholder="Name" />
+          </div>
+        )}
+
         <div className="input">
           <HiOutlineMail className="custom-icon" />
           <input type="email" placeholder="Email Id" />
@@ -25,12 +32,31 @@ const LoginSignup = () => {
           <input type="password" placeholder="Password" />
         </div>
       </div>
-      <div className="forgot-password">
-        Lost password?<span>Click Here</span>
-      </div>
+      {action === "Sign Up" ? (
+        <div></div>
+      ) : (
+        <div className="forgot-password">
+          Lost password?<span>Click Here</span>
+        </div>
+      )}
+
       <div className="submit-container">
-        <div className="submit">Sign Up</div>
-        <div className="submit">Log In</div>
+        <div
+          className={action === "Login" ? "submit gray" : "submit"}
+          onClick={() => {
+            setAction("Sign Up");
+          }}
+        >
+          Sign Up
+        </div>
+        <div
+          className={action === "Sign Up" ? "submit gray" : "submit"}
+          onClick={() => {
+            setAction("Login");
+          }}
+        >
+          Login
+        </div>
       </div>
     </div>
   );
