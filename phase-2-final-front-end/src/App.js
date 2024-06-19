43b72cb3e27b38f44b36044682f4410a075ddd40
Candice Thomason <candice.thomason@gmail.com>
2024-06-19 10:30:37 +1000

    added a TimeLogForm component

--- phase-2-final-front-end/src/App.js
+++ phase-2-final-front-end/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import LoginSignup from "./Components/LoginSignup/LoginSignup";
+import TimeLogForm from "./Components/TimeLogForm/TimeLogForm";
 
 function App() {
   return (
