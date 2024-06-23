ef07f81684016cb8b648dd960c999353c6d61da7
Candice Thomason <candice.thomason@gmail.com>
2024-06-24 09:30:39 +1000

    trying to fix AuthContext.js and AuthProvider.js

--- phase-2-final-front-end/src/AuthContext.js
+++ phase-2-final-front-end/src/AuthContext.js
@@ -1,22 +1,10 @@
-import React, { createContext, useState } from "react";
+// AuthContext.js
+import React, { createContext } from "react";
 
-// Create the AuthContext
-export const AuthContext = createContext();
+const AuthContext = createContext({
+  isLoggedIn: false,
+  login: () => {},
+  logout: () => {},
+});
 
-export const AuthProvider = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  const login = () => {
-    setIsAuthenticated(true);
-  };
-
-  const logout = () => {
-    setIsAuthenticated(false);
-  };
-
-  return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
+export default AuthContext;
