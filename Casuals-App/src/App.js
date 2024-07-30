9639f5b59d7dac6c1ec4bb5cbddc76f08051798b
Candice Thomason <candice.thomason@gmail.com>
2024-07-31 09:18:27 +1000

    added links in navbar

--- Casuals-App/src/App.js
+++ Casuals-App/src/App.js
@@ -10,7 +10,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import LoginSignup from "./Components/LoginSignup/LoginSignup";
 import Dashboard from "./Components/Dashboard/Dashboard";
 import ForgotPassword from "./Components/ForgotPassword/ForgotPassword";
-import Profile from "./Components/Profile/Profile"; // Import Profile component
+import Profile from "./Components/Profile/Profile";
+import About from "./Components/About/About";
+import Verification from "./Components/Verification/Verification";
+import Contact from "./Components/Contact/Contact";
 
 function App() {
   return (
@@ -19,8 +22,10 @@ function App() {
         <Route path="/login" element={<LoginSignup />} />
         <Route path="/forgotpassword" element={<ForgotPassword />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/profile" element={<Profile />} />{" "}
-        {/* Add Profile route */}
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/verification" element={<Verification />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </Router>
--- Casuals-App/src/Components/Header/Header.js
+++ Casuals-App/src/Components/Header/Header.js
@@ -2,15 +2,16 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Header.css";
 import logo from "../Assets/EVSPLwordnologo.png";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="header-container">
       <nav className="navbar navbar-expand-lg navbar-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">
+          <Link className="navbar-brand" to="/dashboard">
             <img src={logo} alt="Logo" className="header-logo" />
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -25,24 +26,28 @@ const Header = () => {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
+                <Link
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/dashboard"
+                >
                   Home
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/about">
+                <Link className="nav-link" to="/about">
                   About
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/services">
-                  Services
-                </a>
+                <Link className="nav-link" to="/verification">
+                  Verification
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/contact">
+                <Link className="nav-link" to="/contact">
                   Contact
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
